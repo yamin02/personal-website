6f44f6f4f5fd0eebd020025de256036c00992d84
yamin02 <yamin136635@gmail.com>
2024-07-25 09:01:05 +0600

    changed the portfolio

--- public/script.js
+++ public/script.js
@@ -1,41 +1,5 @@
 $(function() {
 
-	//===========================
-	//MIXITUP TILES==============
-	//===========================
-	$('#portfolio_grid').mixItUp({
-		animation: {
-			animateResizeContainer: true,
-			animateResizeTargets: true,
-			duration: 700,
-			effects: 'fade rotateZ(0deg) scale(0.01) rotateX(0deg)',
-			// easing: 'cubic-bezier(0.68, -0.55, 0.265, 1.55)'
-		}
-	});
-
-	//================================
-	//ANIMATION=======================
-	//================================
-	//Header of each section animation
-	$(".section_header").animated("fadeInUp", "fadeOutDown");
-
-	//about
-	$(".animation_left").animated("fadeInLeft", "fadeOutDown");
-	$(".animation_center").animated("flipInY", "fadeOutDown");
-	$(".animation_right").animated("fadeInRight", "fadeOutDown");
-
-	//cv
-	$(".left .item").animated("fadeInLeft", "fadeOutDown");
-	$(".right .item").animated("fadeInRight", "fadeOutDown");
-
-	//=======================================================================
-	//SVG Fallback===========================================================
-	//=======================================================================
-	// if(!Modernizr.svg) {
-	// 	$("img[src*='svg']").attr("src", function() {
-	// 		return $(this).attr("src").replace(".svg", ".png");
-	// 	});
-	// };
 
 	//menu button animation================================================
 	$(".toggle_mnu").click(function() {
@@ -72,50 +36,6 @@ $(function() {
 		}
 	});
 
-	//creates id's for project items and links for Magnific popup
-	$(".portfolio_item").each(function(i) {
-		$(this).find("a").attr("href", "#proj" + i);
-		$(this).find(".more").attr("id", "proj" + i);
-	});
-
-	//===========================
-	//Magnific popup=============
-	//===========================
-	//photo image
-	$('.pop_up').magnificPopup({
-		type: 'image',
-		callbacks: {
-			open: function() {
-				$(".toggle_mnu").css("display", "none");
-			},
-			close: function() {
-				$(".toggle_mnu").css("display", "block");
-			}
-		}
-	});
-
-	//project images
-	$('.pop_details').magnificPopup({
-		type: 'inline',
-		midClick: true,
-		showCloseBtn: true,
-		enableEscapeKey: true,
-		// overflowY: "hidden",
-		callbacks: {
-			open: function() {
-				$(".toggle_mnu").css("display", "none");
-			},
-			close: function() {
-				$(".toggle_mnu").css("display", "block");
-			}
-		}
-	});
-
-	//portfolio section add remove class
-	$(".portfolio li").click(function() {
-		$(".portfolio li").removeClass("active");
-		$(this).toggleClass("active");
-	});
 
 	//===================================
 	//Chrome Smooth Scroll===============
@@ -129,10 +49,10 @@ $(function() {
 
 	};
 
-	//prevent images from dragging=============
-	$("img, a").on("dragstart", function(event) {
-		event.preventDefault();
-	});
+	// //prevent images from dragging=============
+	// $("img, a").on("dragstart", function(event) {
+	// 	event.preventDefault();
+	// });
 
 	//========================================
 	//PageScroll2id smoothscroll to link =====
@@ -188,11 +108,8 @@ $(function() {
 });
 
 $(window).load(function() {
-	//preloader settings =========================================
-	$(".loader_image").delay(500).fadeOut();
-	$(".loader").delay(500).fadeOut("slow");
 
-	//animation + waypoints plugin
-	$(".text_wrapper h1").animated("fadeInDown", "fadeOutUp");
-	$(".text_wrapper p").animated("fadeInUp", "fadeOutDown");
+	// //animation + waypoints plugin
+	// $(".text_wrapper h1").animated("fadeInDown", "fadeOutUp");
+	// $(".text_wrapper p").animated("fadeInUp", "fadeOutDown");
 });
